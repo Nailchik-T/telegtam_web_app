524617f6e3acce64e82a883ea5f4c7b5ade5077b
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2024-05-26 15:54:03 +0600

    asd

--- src/pages/productDetail/ProductDetail.jsx
+++ src/pages/productDetail/ProductDetail.jsx
@@ -3,43 +3,43 @@ import Button from '../../components/Button/Button'
 
 const ProductDetail = () => {
 	return (
-		<>
+		<div className='flex flex-col' style={{}}>
 			<div
 				style={{ borderRadius: 50 }}
-				className='flex flex-col items-center p-5 bg-white mt-24'
+				className='flex flex-col items-center  bg-white mt-24'
 			>
-				<div className=' absolute top-24 p-5 shadow-custom flex justify-center items-center rounded-2xl w-80 h-80 bg-secondary'>
+				<div className=' absolute top-24 p-5 shadow-custom flex justify-center items-center rounded-2xl w-80 h-80 bg-secondary '>
 					<img
 						src={icon}
 						alt='Centered Image'
 						className=' max-w-full max-h-full  '
 					/>
 				</div>
-				<div className='relative mt-64 px-6'>
+				<div className='relative mt-64 px-6 mb-32'>
 					<div className='font-bold text-2xl'>LOREM IPSUM</div>
-					<div className='mt-2 text-sm font-light text-primary'>
-						Lorem ipsum dolor sit
+					<div className='mt-2 text-sm font-bold text-primary'>
+						Мужские кеды
 					</div>
-					<p className='text-sm font-light mt-2 text-primary  w-full  '>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit ыфвыф.
-						Suscipit qui neque consequuntur officia reprehenderit id quae
-						laboriosam! Aut magni minus repudiandae deleniti ab sed pariatur
-						porro, id perferendis omnis fugiat?
+					<p className='text-sm font-light mt-2 text-primary  w-full mb-5  '>
+						Мужские кеды из натуральной кожи ECCO STREET LITE M передают
+						настоящую гармонию стиля и практичности. За лаконичным дизайном
+						скрываются передовые технологии комфорта для максимальной свободы
+						движения.
 					</p>
 				</div>
 			</div>
-			<div className='flex justify-between p-7 items-center text-white bg-primary rounded-t-3xl'>
+			<div className='fixed flex w-full bottom-0 justify-between p-7 m-t items-center text-white bg-primary rounded-t-3xl'>
 				<div className='flex flex-col'>
-					<p className='font-extralight'>Price</p>
-					<p className='font-bold'>$ 100</p>
+					<p className='font-extralight'>Цена</p>
+					<p className='font-bold'>12 500 ₸</p>
 				</div>
 				<div>
-					<Button className=' rounded-2xl px-10 py-4 bg-primaryDep text-white hover:bg-slate-800'>
-						Add to Bag
+					<Button className=' font-bold rounded-2xl px-10 py-4 bg-primaryDep text-white hover:bg-slate-800'>
+						Добавить в корзину
 					</Button>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
