5d568500618cd2af6db3407026aee92e8e6f31f8
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2024-05-27 11:55:55 +0600

    asdsad

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -11,7 +11,7 @@ export const Catalog = () => {
 	useEffect(() => {
 		const fetchProducts = async () => {
 			try {
-				const response = await axios.get('http://localhost:5000/api/products')
+				const response = await axios.get('http://127.0.0.1:5000/api/products')
 				setProducts(response.data)
 			} catch (err) {
 				setError(err.message)
